dadde5c6937be3087150bd68add56a12c5116dde
andrewlawrence <andrewlaw122@gmail.com>
2025-04-29 18:02:21 -0400

    Ignore macOS .DS_Store files

--- AVA_Photo_App/AppDelegate.swift
+++ AVA_Photo_App/AppDelegate.swift
@@ -36,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "AVA_Photo_App") // <-- Match your .xcdatamodeld filename
+        let container = NSPersistentContainer(name: "AVA_Photo_App") 
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
