a3c1d905642cf9684593274cc8f8bddb80039113
Valva <valerie.edokpa@gmail.com>
2025-04-29 20:07:49 -0400

    MAP STUFF

--- AVA_Photo_App/MapViewController.swift
+++ AVA_Photo_App/MapViewController.swift
@@ -8,19 +8,28 @@
 import Foundation
 import UIKit
 import MapKit
-//import CoreLocation
+import CoreLocation
 import CoreData
 
-class MapViewController:UIViewController, MKMapViewDelegate {
+class MapViewController:UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
    /*
     - Controls the map and adds the pins (annoations)
     */
     
+    //Connects to the MapView from the storyboard
     @IBOutlet weak var mapView: MKMapView!
+    
+    //This array holds all the places loaded from the Core Data --> where all the user's saved places will be displayed
+    var places: [Place] = []
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //let's set it up
+        //Set the delegate of the map view to be the map view controller
+        mapView.delegate = self
+        
         
         
     }
